1a2e3ccc827af29c8eec2a3e4bad008bb2e251f0
livia-pimentel <livia_macedo89@yahoo.com.br>
2025-12-27 09:21:57 -0300

    Usando stream para pegar todas numero das temporadas de cada episodio

--- src/main/java/br/com/alura/screenmatch/principal/Main.java
+++ src/main/java/br/com/alura/screenmatch/principal/Main.java
@@ -3,13 +3,11 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSeries;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -68,7 +66,20 @@ public class Main {
                 .collect(Collectors.toList());
 //                .toList(); // Lista imutavel
 
+        // Classificando os 5 melhores episodios pela avaliação
+        System.out.println("\nTop 5 episódios: ");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
 
+        // Pegando os episodios com as informações da temporada
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numeroTemporada(), d))
+                ).collect(Collectors.toList());
 
+        episodios.forEach(System.out::println);
     }
 }
